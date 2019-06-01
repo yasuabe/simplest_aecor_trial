54eb0e96603c3cbd19ba3b86d35f8686a2287239
Yasuyuki Abe <yasu@yasuabe.net>
2019-06-01 17:42:01 +0900

    separate ofsetstore from journal

--- modules/simplest/common/src/main/scala/simplest/infra/PostgresJournal.scala
+++ modules/simplest/common/src/main/scala/simplest/infra/PostgresJournal.scala
@@ -1,21 +1,16 @@
 package simplest.infra
 
 import aecor.data._
-import aecor.distributedprocessing.DistributedProcessing.Process
+import aecor.journal.postgres.PostgresEventJournal
 import aecor.journal.postgres.PostgresEventJournal.Serializer.TypeHint
-import aecor.journal.postgres.{Offset, PostgresEventJournal, PostgresOffsetStore}
-import aecor.runtime.KeyValueStore
-import cats.effect.{Async, Concurrent, ContextShift, Timer}
+import cats.effect.{Async, ContextShift}
 import cats.syntax.either._
 import cats.syntax.functor._
 import doobie.util.transactor.Transactor
 import simplest.common.protobuf.msg
 import simplest.model.{IncrementEvent, NumberAdded, NumberCreated}
-import simplest.util.streamToProcess
 
-import scala.concurrent.duration._
-
-class PostgresJournal[F[_]: Async: ContextShift] {
+class PostgresJournal[F[_]: Async: ContextShift] { // TODO このクラスの意味再考
   import PostgresJournal._
 
   lazy val transactor: Transactor[F] = Transactor.fromDriverManager[F](
@@ -31,24 +26,6 @@ class PostgresJournal[F[_]: Async: ContextShift] {
       PostgresJournal.tagging,
       IncrementEventSerializer
     )
-  val offsetStore = PostgresOffsetStore("consumer_offset")
-
-  lazy val offsetStoreCIO: KeyValueStore[F, TagConsumer, Offset] =
-    offsetStore mapK transactor.trans
-
-  def createProcesses(
-    sink: fs2.Sink[F, Committable[F, IncrementEntityEvent]]
-  )(implicit F: Concurrent[F], G: Timer[F]): List[Process[F]] = {
-
-    val queries = journal.queries(100.millis).withOffsetStore(offsetStoreCIO)
-    def tagStream(tag: EventTag): fs2.Stream[F, Unit] =
-      fs2.Stream
-        .force(queries.eventsByTag(tag, ConsumerId("ViewProjection")))
-        .map(_.map { case (_, event) => event })
-        .through(sink)
-
-    PostgresJournal.tagging.tags.map(t => streamToProcess(tagStream(t)))
-  }
 }
 
 object PostgresJournal {
--- modules/simplest/readerSide/src/main/scala/simplest/readside/ReaderMain.scala
+++ modules/simplest/readerSide/src/main/scala/simplest/readside/ReaderMain.scala
@@ -1,18 +1,45 @@
 package simplest.readside
 
+import aecor.data.{Committable, ConsumerId, EventTag, TagConsumer}
+
 import scala.io.StdIn
 import aecor.distributedprocessing.DistributedProcessing
 import aecor.distributedprocessing.DistributedProcessing.Process
-import cats.effect.{Concurrent, ContextShift, ExitCode, Timer}
+import aecor.journal.postgres.{Offset, PostgresOffsetStore}
+import aecor.runtime.KeyValueStore
+import cats.effect.{Async, Concurrent, ContextShift, ExitCode, Timer}
+import doobie.util.transactor.Transactor
 import monix.eval.{Task, TaskApp}
 import simplest.infra._
-import simplest.readside.impl.{IncrementViewRepoImpl, IncrementProjectionImpl, projectionSink}
-import simplest.util.UsingActorSystem
+import simplest.readside.impl.{IncrementProjectionImpl, IncrementViewRepoImpl, projectionSink}
+import simplest.util.{UsingActorSystem, streamToProcess}
+
+import scala.concurrent.duration._
 
 object ReaderMain extends TaskApp with UsingActorSystem {
+  def createProcesses[F[_]](
+    journal: IncrementJournal[F],
+    offsetStore: KeyValueStore[F, TagConsumer, Offset],
+    sink: fs2.Sink[F, Committable[F, IncrementEntityEvent]]
+  )(implicit F: Concurrent[F], G: Timer[F]): List[Process[F]] = {
+    val queries = journal.queries(100.millis).withOffsetStore(offsetStore)
+
+    def tagStream(tag: EventTag): fs2.Stream[F, Unit] = {
+      val eventStream = queries.eventsByTag(tag, ConsumerId("ViewProjection"))
+      fs2.Stream
+        .force(eventStream)
+        .map(_.map { case (_, event) => event })
+        .through(sink)
+    }
+    PostgresJournal.tagging.tags.map(t => streamToProcess(tagStream(t)))
+  }
+  lazy val offsetStoreCIO = PostgresOffsetStore("consumer_offset")
+  def offsetStore[F[_]: Async](transactor: Transactor[F]): KeyValueStore[F, TagConsumer, Offset] =
+    offsetStoreCIO mapK transactor.trans
+
   def processes[F[_]: Timer: Concurrent: ContextShift](journal: PostgresJournal[F]): List[Process[F]] = {
-    val projection = new IncrementProjectionImpl[F](new IncrementViewRepoImpl[F](journal.transactor))
-    journal.createProcesses(projectionSink(projection))
+    val projection = new IncrementProjectionImpl[F](new IncrementViewRepoImpl[F](journal.transactor)) // TODO ワイヤリング考える
+    createProcesses(journal.journal, offsetStore(journal.transactor), projectionSink(projection))
   }
 
   def run(args: List[String]): Task[ExitCode] = {
@@ -21,9 +48,9 @@ object ReaderMain extends TaskApp with UsingActorSystem {
     import cats.temp.par._
     import doobie.syntax.connectionio._
     actorSystem("increment") use { actorSys =>
-      val j = new PostgresJournal[Task]
+      val j = new PostgresJournal[Task] // TODO 変数名
       for {
-        _ <-  List(j.journal.createTable, j.offsetStore.createTable.transact(j.transactor)).parSequence
+        _ <-  List(j.journal.createTable, offsetStoreCIO.createTable.transact(j.transactor)).parSequence // TODO 初期化プロセスまとめる
         killSwitch <- DistributedProcessing(actorSys).start("ViewProjectionProcessing", processes[Task](j))
         _ <- Task {
           StdIn.readLine("press [ENTER]")
